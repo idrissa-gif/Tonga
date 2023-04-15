e88b69f5bc499c359cb630d5a45813a500048065
idrissa-gif <idrissa@iut-dhaka.edu>
2023-04-15 15:14:50 +0600

    create client website

---
+++ src/main/java/com/visitafrica/tongaclient/TongaClientApplication.java
@@ -0,0 +1,13 @@
+package com.visitafrica.tongaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TongaClientApplication.class, args);
+    }
+
+}
